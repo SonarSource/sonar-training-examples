b829260bf850f1bf403ada5ea821dec4782824f7
Olivier Korach <olivier.korach@gmail.com>
2018-09-30 12:05:36 +0200

    fixes #5, fixes #4 Adds the branch example

---
+++ branches/README.md
@@ -0,0 +1,26 @@
+# Branches
+
+## Use case
+This example demonstrates:
+- Branches in general
+- Difference between short lived branches and long lived branches
+- Difference of SQ 5.6 _old-style_ branches and SQ 6.7 _new-style_ branches
+
+## Usage
+
+Run `./analyze-branches.sh`
+
+This will:
+- Delete the project key **training:branches** if it exists in SonarQube (to start from a scratch)
+- Create a project with name **Training: Branches** with a master branch
+- One long lived branch name release-1.1 when some new bugs were injected
+- 2 short lived branches, one feature branch on **master** and one hotfix on **release-1.1**, each of them with new issues compared to their target branches
+
+You can also run `./analyze-branches.sh sonar.branch` to run the same branches analysis with the old branch system to show the difference. This will create 4 different projects,
+one for each branch, without any visibility of the delta between branches.
+To not conflict with the new style branches projects the project key is **training:branches-old-style**, and project name is **Training: Branches (old-style)**
+
+## TODO
+- Mark an issue from master as FP, and show that this is reported on all the other branches (feature and release-1.1)
+- Display scanner commands on stdout to make it clearer for readers
+
---
+++ branches/analyze-branches.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+FILE="src/helloworld/HelloWorld.java"
+PK="training:branches"
+PN="Training: Branches"
+BRANCH_PROP="sonar.branch.name"
+TARGET_PROP="sonar.branch.target"
+
+if [ "$1" == "-h" ]; then
+   echo "$0 [sonar.branch] [-h]"
+   echo "The default branch property is sonar.branch.name, but you can also pass sonar.branch"
+   echo "to show the effect in SonarQube"
+   echo "When using sonar.branch.name, the projetc key is training:branches"
+   echo "When using sonar.branch, the projetc key is training:branches-old-style"
+fi
+if [ "$1" == "sonar.branch" ]; then
+   PK="$PK-old-style"
+   PN="$PN (old-style)"
+   BRANCH_PROP="sonar.branch"
+   TARGET_PROP="sonar.foo"
+fi
+if [ "$SQ_URL" != "" ]; then
+   sqHostOpt="-Dsonar.host.url=$SQ_URL"
+fi
+if [ "$TOKEN" != "" ]; then
+   sqLoginOpt="-Dsonar.login=$TOKEN"
+fi
+
+PARAMS="$sqHostOpt $sqLoginOpt -Dsonar.projectKey=$PK"
+
+
+cp $FILE.orig $FILE
+
+# Delete existing project
+curl -X POST -u $TOKEN: $SQ_URL/api/projects/delete?project=$PK
+
+# Scan initial master branch
+sonar-scanner $PARAMS -Dsonar.projectName="$PN" -Dsonar.projectVersion=1.0 -Dsonar.projectDate=2018-06-01
+
+# Release branch
+# Scan the baseline code from the branched master
+sonar-scanner $PARAMS -D$BRANCH_PROP=release-1.1
+
+# Modify code, delete LoCs, inject 1 issue, add other LoCs
+cat $FILE.orig | grep -v LINE-DELETE-RELEASE | sed -e 's/ISSUE-RELEASE/FIXME to fix for the release/' | sed -e 's/\/\/ LINE-ADD-RELEASE //g' > $FILE
+# Scan after changes
+sonar-scanner $PARAMS -Dsonar.projectName="$PN" -D$BRANCH_PROP=release-1.1
+# Backup file
+cp $FILE $FILE.release.$$
+
+# Change master branch and scan with incremented version
+# Modify code, inject 1 issue, add LoCs
+cat $FILE.orig | grep -v LINE-DELETE-MASTER | sed -e 's/ISSUE-MASTER/FIXME to fix on master/' | sed -e 's/\/\/ LINE-ADD-MASTER //g' > $FILE
+sonar-scanner $PARAMS -Dsonar.projectName="$PN" -Dsonar.projectVersion=2.0
+cp $FILE $FILE.master.$$
+
+# Create feature branch on master
+# Modify code, inject 1 issue, add LoCs
+cat $FILE.master.$$ | grep -v LINE-DELETE-FEATURE | sed -e 's/ISSUE-FEATURE/FIXME to fix on feature branch/' | sed -e 's/\/\/ LINE-ADD-FEATURE //g' > $FILE
+sonar-scanner $PARAMS -Dsonar.projectName="$PN" -D$BRANCH_PROP=feature-X
+cp $FILE $FILE.feature.$$
+
+# Create feature branch on release branch
+# Fix release issues
+cat $FILE.release.$$ | grep -v FIXME > $FILE
+# Inject one new issue
+echo "// FIXME issue introduced in hotfix" >> $FILE
+sonar-scanner $PARAMS -Dsonar.projectName="$PN" -D$BRANCH_PROP=hotfix-for-release -D$TARGET_PROP=release-1.1
+
+rm -f $FILE.*.$$
