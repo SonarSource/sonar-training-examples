1a56f7daec988ea74b608a134d7ba782e56da13a
Brian Cipollone <brian.cipollone@sonarsource.com>
2018-10-05 15:28:05 -0500

    Convert tabs to spaces

--- security/src/main/java/training/security/Insecure.java
+++ security/src/main/java/training/security/Insecure.java
@@ -21,54 +21,54 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Insecure {
 
-	public void badFunction(HttpServletRequest request) throws IOException {
-		String obj = request.getParameter("data");
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.enableDefaultTyping();
-		String val = mapper.readValue(obj, String.class);
-		File tempDir;
-		tempDir = File.createTempFile("", ".");
-		tempDir.delete();
-		tempDir.mkdir();
-		Files.exists(Paths.get("/tmp/", obj));
-	}
+  public void badFunction(HttpServletRequest request) throws IOException {
+    String obj = request.getParameter("data");
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.enableDefaultTyping();
+    String val = mapper.readValue(obj, String.class);
+    File tempDir;
+    tempDir = File.createTempFile("", ".");
+    tempDir.delete();
+    tempDir.mkdir();
+    Files.exists(Paths.get("/tmp/", obj));
+  }
 
-	public void hackRequest(HttpServletRequest request, Connection connection) {
-		String user = request.getParameter("user");
-		String password = "password";
-		password = request.getParameter("pass");
-		String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + password + "'";
-		try {
-			java.sql.Statement statement = connection.createStatement();
-			java.sql.ResultSet resultSet = statement.executeQuery(query);
-			if (resultSet.next()) {
-				Object obj = new Object();
-				HashMap map = new HashMap<>();
-				Enumeration names = request.getParameterNames();
-				while (names.hasMoreElements()) {
-					String name = (String) names.nextElement();
-					map.put(name, request.getParameterValues(name));
-				}
-				BeanUtils.populate(obj, map);
-			}
-		} catch (Exception e) {
-		}
-	}
+  public void hackRequest(HttpServletRequest request, Connection connection) {
+    String user = request.getParameter("user");
+    String password = "password";
+    password = request.getParameter("pass");
+    String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + password + "'";
+    try {
+      java.sql.Statement statement = connection.createStatement();
+      java.sql.ResultSet resultSet = statement.executeQuery(query);
+      if (resultSet.next()) {
+        Object obj = new Object();
+        HashMap map = new HashMap<>();
+        Enumeration names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+          String name = (String) names.nextElement();
+          map.put(name, request.getParameterValues(name));
+        }
+        BeanUtils.populate(obj, map);
+      }
+    } catch (Exception e) {
+    }
+  }
 
-	public void modResponse(HttpServletResponse response) {
-		Cookie c = new Cookie("SECRET", "SECRET");
-		response.addCookie(c);
-	}
+  public void modResponse(HttpServletResponse response) {
+    Cookie c = new Cookie("SECRET", "SECRET");
+    response.addCookie(c);
+  }
 
-	public KeyPair weakKey() {
-		KeyPairGenerator keyPairGen;
-		try {
-			keyPairGen = KeyPairGenerator.getInstance("RSA");
-			keyPairGen.initialize(512);
-			return keyPairGen.genKeyPair();
-		} catch (NoSuchAlgorithmException e) {
-			return null;
-		}
-	}
+  public KeyPair weakKey() {
+    KeyPairGenerator keyPairGen;
+    try {
+      keyPairGen = KeyPairGenerator.getInstance("RSA");
+      keyPairGen.initialize(512);
+      return keyPairGen.genKeyPair();
+    } catch (NoSuchAlgorithmException e) {
+      return null;
+    }
+  }
 
 }
