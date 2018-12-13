14599a78ae2e189d572401390e6c8f63bb39a1fc
Brian Cipollone <brian.cipollone@sonarsource.com>
2018-12-13 15:26:02 -0600

    Really fix run

--- security/run.sh
+++ security/run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 PK="training:security"
+HOST=http://localhost:9000
+
 
 if [ "$SQ_URL" != "" ]; then
-   sqHostOpt="-Dsonar.host.url=$SQ_URL"
+   HOST=$SQ_URL
 fi
+
+sqHostOpt="-Dsonar.host.url=$SQ_URL"
+
 if [ "$TOKEN" != "" ]; then
    sqLoginOpt="-Dsonar.login=$TOKEN"
+   echo "Deleting project"
+   curl -X POST $curlLoginOpt $HOST/api/projects/delete?project=$PK
 fi
 
-echo "Deleting project"
-
-curl -X POST -u $sqHostOpt $sqLoginOpt $SQ_URL/api/projects/delete?project=$PK
-
-echo "Creating project"
+echo "Running analysis"
 mvn clean verify sonar:sonar $sqHostOpt $sqLoginOpt
