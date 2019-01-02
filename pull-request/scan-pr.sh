a19b8e946a3697cd6c0e87b106e3bf20fa1ca5fb
Olivier Korach <olivier.korach@gmail.com>
2019-01-02 17:57:28 +0100

    new defects

--- pull-request/scan-pr.sh
+++ pull-request/scan-pr.sh
@@ -2,9 +2,10 @@
 #!/bin/bash
 
 PR=$1
+PR_NAME=$2
 PARAMS="-Dsonar.host.url=$SQ_URL -Dsonar.login=$TOKEN"
 
 # Scan initial master branch
-git checkout origin/pr-demo
+# git checkout origin/pr-demo
 
-sonar-scanner $PARAMS -Dsonar.pullrequest.branch=pr-demo -Dsonar.pullrequest.key=$PR
+sonar-scanner $PARAMS -Dsonar.pullrequest.branch=$PR_NAME -Dsonar.pullrequest.key=$PR
