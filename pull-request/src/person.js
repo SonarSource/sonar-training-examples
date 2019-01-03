a54b99d61612997878312b5a986ae0a1d735a15c
Olivier Korach <olivier.korach@gmail.com>
2019-01-03 08:48:44 +0100

    Add person object

---
+++ pull-request/src/person.js
@@ -0,0 +1,15 @@
+function Person(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = undefined;
+    this.eyeColor = undefined;
+    this.nationality = undefined;
+}
+
+Person.prototype.fullname = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+Person.prototype.wishHappyBirthday = function() {
+    return "Happy birthday " + this.firstName;
+}
