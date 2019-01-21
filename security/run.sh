31cc26941c3897718b08cc42d0e4c8369064ced3
Olivier Korach <olivier.korach@gmail.com>
2019-01-21 18:35:06 +0100

    Leverage common SQ_URL

--- security/run.sh
+++ security/run.sh
@@ -6,7 +6,7 @@
 PK="training:security"
 
 echo "Deleting project"
-curl -X POST -u $SQ_TOKEN: $SQ_HOST/api/projects/delete?project=$PK
+curl -X POST -u $SQ_TOKEN: $SQ_URL/api/projects/delete?project=$PK
 
 echo "Running analysis"
-mvn clean verify sonar:sonar -Dsonar.host.url=$SQ_HOST -Dsonar.login=$SQ_TOKEN -Dsonar.projectKey=$PK
+mvn clean verify sonar:sonar -Dsonar.host.url=$SQ_URL -Dsonar.login=$SQ_TOKEN -Dsonar.projectKey=$PK
