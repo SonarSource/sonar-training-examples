23686643d97822f421d13736838cb2111a99835b
Olivier Korach <olivier.korach@gmail.com>
2019-01-21 18:35:26 +0100

    Leverage common SQ_TOKEN

--- portfolios/tagAllProjects.sh
+++ portfolios/tagAllProjects.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Load common environment
+. ../sqlib.sh
+
 tag="superbank.com"
 
 KEYS="BANK-RETAIL-401K WEBSITE-TIER2-BIZLOGIC"
@@ -11,11 +14,11 @@ for p in `ls | grep -e tier -e bank -e insurance`; do
 	if [ -f "$p/sonar-project.properties" ]; then
       # We extract the key from sonar-project.properties
 		pk=`grep sonar.projectKey $p/sonar-project.properties | cut -d '=' -f 2`
-	   echo curl -X POST -u '$TOKEN:' "$SQ_URL/api/project_tags/set?project=$pk&tags=superbank.com"
-	   curl -X POST -u $TOKEN: "$SQ_URL/api/project_tags/set?project=$pk&tags=superbank.com"
+	   echo curl -X POST -u '$SQ_TOKEN:' "$SQ_URL/api/project_tags/set?project=$pk&tags=$tag"
+	   curl -X POST -u $SQ_TOKEN: "$SQ_URL/api/project_tags/set?project=$pk&tags=$tag"
    fi
 done
 for pk in $KEYS; do
-	echo curl -X POST -u '$TOKEN:' "$SQ_URL/api/project_tags/set?project=$pk&tags=superbank.com"
-	curl -X POST -u $TOKEN: "$SQ_URL/api/project_tags/set?project=$pk&tags=superbank.com"
+	echo curl -X POST -u '$SQ_TOKEN:' "$SQ_URL/api/project_tags/set?project=$pk&tags=$tag"
+	curl -X POST -u $TOKEN: "$SQ_URL/api/project_tags/set?project=$pk&tags=$tag"
 done
