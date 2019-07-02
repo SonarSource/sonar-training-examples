421dd1ae22f9c4c06a2176775aa95a82c019fe54
Olivier Korach <olivier.korach@gmail.com>
2019-07-02 19:04:47 +0200

    Example of custom sources/sanitizers/sinks (#59)
    * Add custom sources/sanitizers/sinks properties
    
    * Custom sources/sanitizers/sinks config file
    
    * Added fake methods for sources/sanitizers/sinks
    
    * Update for custom security example
    
    * Improved readme
    
    * Add link to JSON secuirty config file

--- security/run.sh
+++ security/run.sh
@@ -9,4 +9,8 @@ echo "Deleting project"
 curl -X POST -u $SQ_TOKEN: $SQ_URL/api/projects/delete?project=$PK
 
 echo "Running analysis"
-mvn clean verify sonar:sonar -Dsonar.host.url=$SQ_URL -Dsonar.login=$SQ_TOKEN -Dsonar.projectKey=$PK
+mvn clean verify sonar:sonar -Dsonar.host.url=$SQ_URL -Dsonar.login=$SQ_TOKEN -Dsonar.projectKey=$PK \
+   -Dsonar.scm.disabled=true -X \
+   -Dsonar.security.sources.javasecurity.S3649=s3649JavaSqlInjectionConfig.json \
+   -Dsonar.security.sanitizers.javasecurity.S3649=s3649JavaSqlInjectionConfig.json \
+   -Dsonar.security.sinks.javasecurity.S3649=s3649JavaSqlInjectionConfig.json
