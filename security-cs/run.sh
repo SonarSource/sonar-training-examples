6a8675dab2c60454aeeac6ef1b38095d13544e1d
sylvain-combe-sonarsource <donotshow@local.local>
2020-02-12 15:08:29 +0100

    Use of SonarQube token for analysis in run.sh

--- security-cs/run.sh
+++ security-cs/run.sh
@@ -5,16 +5,19 @@
 
 PK="training:security-cs"
 
+
+echo "SQ_URL/SQ_TOKEN $SQ_URL / $SQ_TOKEN"
+
 doScan() {
-  dotnet sonarscanner begin /d:sonar.host.url=$SQ_URL /k:$PK /n:"C# Security Training Example"
+  dotnet sonarscanner begin /d:sonar.host.url=$SQ_URL /d:sonar.login=$SQ_TOKEN /k:$PK /n:"C# Security Training Example"
   dotnet build My_API.sln
-  dotnet sonarscanner end
+  dotnet sonarscanner end /d:sonar.login=$SQ_TOKEN
 }
 
 pushd My_API
 
 echo "Deleting project"
-curl -X POST -u $SQ_TOKEN: $SQ_URL/api/projects/delete?project=$PK
+curl -X POST --silent --output /dev/null --show-error -u $SQ_TOKEN: "$SQ_URL/api/projects/delete?project=$PK"
 
 echo "Running initial project scan"
 doScan
