a8a56c271b634ca0e56e2c7eee6d537be75a1ffe
Olivier Korach <olivier.korach@gmail.com>
2020-10-28 17:18:00 +0100

    Follow standard SONAR env vars

--- security-cs/run.sh
+++ security-cs/run.sh
@@ -6,15 +6,15 @@
 PK="training:security-cs"
 
 doScan() {
-  dotnet sonarscanner begin /d:sonar.host.url=$SQ_URL /k:$PK /n:"C# Security Training Example"
+  dotnet sonarscanner begin /d:sonar.host.url=$SONAR_HOST_URL /k:$PK /n:"C# Security Training Example" /d:sonar.login=$SONAR_TOKEN
   dotnet build My_API.sln
-  dotnet sonarscanner end
+  dotnet sonarscanner end /d:sonar.login=$SONAR_TOKEN
 }
 
 pushd My_API
 
 echo "Deleting project"
-curl -X POST -u $SQ_TOKEN: $SQ_URL/api/projects/delete?project=$PK
+curl -X POST -u $SONAR_TOKEN: $SONAR_URL/api/projects/delete?project=$PK
 
 echo "Running initial project scan"
 doScan
@@ -22,16 +22,16 @@ doScan
 echo "Simulating change that introduces vulnerabilities"
 pushd My_API/Controllers
 cp ValuesController.cs ValuesController.cs.orig
-cp ValuesController.cs.newCode ValuesController.cs 
-mv ValuesDao.cs.newCode ValuesDao.cs 
+cp ValuesController.cs.newCode ValuesController.cs
+mv ValuesDao.cs.newCode ValuesDao.cs
 popd
 
 echo "Running follow-up project scan"
 doScan
 
-# restore things as they were 
+# restore things as they were
 pushd My_API/Controllers
-mv ValuesController.cs.orig ValuesController.cs 
+mv ValuesController.cs.orig ValuesController.cs
 mv ValuesDao.cs ValuesDao.cs.newCode
 popd
 
