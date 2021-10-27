afaa23b18453d0610e5d359b2e35eb69bed8e60f
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 21:08:22 +0200

    Fix comment of test

--- coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
+++ coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
@@ -8,8 +8,8 @@ public class CoverageMetricsTest {
   public void test1() throws Exception {
     // This test covers all the lines of the c.f() method BUT
     // does not cover all the conditions. Running that test alone
-    // yields a 100% (6/6) line coverage BUT only a 50% (1/2) condition coverage
-    // This produces an overall coverage of (6+1)/(6+2)=87% overall coverage
+    // yields a 100% (5/5) line coverage BUT only a 50% (1/2) condition coverage
+    // This produces an overall coverage of (5+1)/(5+2)=85.7% overall coverage
     CoverageMetrics c = new CoverageMetrics();
     assertEquals(0.5, c.f(1), 0.0);
   }
