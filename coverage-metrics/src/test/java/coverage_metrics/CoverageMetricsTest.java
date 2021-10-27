3d15b7cd9aefa07d2e4aa71e148a911cfe34f4d3
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 21:04:18 +0200

    Comment test better

--- coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
+++ coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
@@ -25,10 +25,10 @@ public class CoverageMetricsTest {
 
   @Test
   public void test3() {
-    CoverageMetrics c = new CoverageMetrics();
-    // The below line is an incorrect test: Method c.f() is invoked
+    // This test is an example of incorrect test: Method c.f() is invoked
     // but the returned value is not verified to be as expected
     // SonarQube will raise an issue for lack of assertion
+    CoverageMetrics c = new CoverageMetrics();
     float x = c.f(5);
   }
 }
