e62c832859f55426f25ac4a2372ef4162704078c
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 21:02:00 +0200

    Comment test

--- coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
+++ coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 public class CoverageMetricsTest {
   @Test
   public void test1() throws Exception {
+    // This test covers all the lines of the c.f() method BUT
+    // does not cover all the conditions. Running that test alone
+    // yields a 100% (6/6) line coverage BUT only a 50% (1/2) condition coverage
+    // This produces an overall coverage of (6+1)/(6+2)=87% overall coverage
     CoverageMetrics c = new CoverageMetrics();
     assertEquals(0.5, c.f(1), 0.0);
   }
