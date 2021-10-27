b8a2b63a3122fcfba3757b87c462ac18bbf3bdb4
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 21:07:34 +0200

    Change tests

--- coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
+++ coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
@@ -5,30 +5,12 @@ import org.junit.Test;
 
 public class CoverageMetricsTest {
   @Test
-  public void test1() throws Exception {
-    // This test covers all the lines of the c.f() method BUT
-    // does not cover all the conditions. Running that test alone
-    // yields a 100% (6/6) line coverage BUT only a 50% (1/2) condition coverage
-    // This produces an overall coverage of (6+1)/(6+2)=87% overall coverage
-    CoverageMetrics c = new CoverageMetrics();
-    assertEquals(0.5, c.f(1), 0.0);
-  }
-
-  @Test
   public void test2() throws Exception {
-    // This 2nd test covers the 2nd condition to achieve (combined with test1)
-    // a 100% line coverage and 100% condition coverage, therefore a
-    // 100% overall coverage
+    // This 2nd test alone covers only 4 out of 5 lines (so line coverage is 80%)
+    // and 1 out of 2 conditions (so condition coverage is 50%)
+    // The overall coverage is (4+1)/(5+2) = 71.4%
     CoverageMetrics c = new CoverageMetrics();
     assertEquals(0.0, c.f(0), 0.0);
   }
 
-  @Test
-  public void test3() {
-    // This test is an example of incorrect test: Method c.f() is invoked
-    // but the returned value is not verified to be as expected
-    // SonarQube will raise an issue for lack of assertion
-    CoverageMetrics c = new CoverageMetrics();
-    float x = c.f(5);
-  }
 }
