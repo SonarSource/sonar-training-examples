f3ba4a36aef0b4190efd58516026e9623c4b5560
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 21:16:50 +0200

    Simplify build script

--- coverage-metrics/build.sh
+++ coverage-metrics/build.sh
@@ -3,22 +3,6 @@
 # Load common environment
 . ../sqlib.sh
 
-testFile="src/test/java/coverage_metrics/CoverageMetricsTest.java"
-
-if [ "$1" = "-h" ] || [ "$1" = "-?" ]; then
-	echo "Usage: $0 [-h|-?] [1|2|all] [<scanner-properties>]"
-	echo "1: Run only UT1"
-	echo "2: Run only UT2"
-	echo "all: Run all UT"
-	echo ""
-	echo "Example: $0 all -Dsonar.host.url=http://localhost:9673"
-	exit 0
-elif [ "$1" = "1" ] || [ "$1" = "2" ] || [ "$1" = "all" ]; then
-	ut=$1
-	shift
-	cp $testFile.ut$ut $testFile
-fi
-
 for branch in master partial-coverage partial-coverage-2 issue-on-test-files
 do
 	git checkout $branch
@@ -31,6 +15,6 @@ do
 		sonar:sonar $branchOpt \
 		-Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN \
 		-Dsonar.exclusions=pom.xml \
-		-Dsonar.projectKey="training:test-ut$ut" -Dsonar.projectName="Training: Coverage UT $ut" $*
+		-Dsonar.projectKey="training:coverage" -Dsonar.projectName="Training: Coverage" $*
 done
 exit $?
