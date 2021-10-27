e02816c3048283a8612fabfc4562a53c473edd61
Olivier Korach <olivier.korach@gmail.com>
2021-10-27 20:57:40 +0200

    Incorrect test to trigger an issue

--- coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
+++ coverage-metrics/src/test/java/coverage_metrics/CoverageMetricsTest.java
@@ -5,20 +5,11 @@ import org.junit.Test;
 
 public class CoverageMetricsTest {
   @Test
-  public void test1() throws Exception {
-    CoverageMetrics c = new CoverageMetrics();
-    assertEquals(0.5, c.f(1), 0.0);
-  }
-
-  @Test
-  public void test2() throws Exception {
-    CoverageMetrics c = new CoverageMetrics();
-    assertEquals(0.0, c.f(0), 0.0);
-  }
-
-  @Test
   public void test3() {
     CoverageMetrics c = new CoverageMetrics();
+    // The below line is an incorrect test: Method c.f() is invoked
+    // but the returned value is not verified to be as expected
+    // SonarQube will raise an issue for lack of assertion
     float x = c.f(5);
   }
 }
