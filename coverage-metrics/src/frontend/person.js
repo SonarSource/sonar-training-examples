d27ba503cbdbe49a0d8d09f27639a917a685f982
Olivier K <161828284+okorach-sonar@users.noreply.github.com>
2024-12-17 10:08:03 +0100

    Add non java files in a branch (#167)
    * Add JS files
    
    * Remove useless files
    
    ---------
    
    Co-authored-by: Olivier Korach <olivier.korach@gmail.com>

---
+++ coverage-metrics/src/frontend/person.js
@@ -0,0 +1,15 @@
+function Person(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = null;
+    this.eyeColor = null;
+    this.nationality = null;
+}
+
+Person.prototype.fullname = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+Person.prototype.wishHappyBirthday = function() {
+    return "Happy birthday " + this.firstName;
+}
