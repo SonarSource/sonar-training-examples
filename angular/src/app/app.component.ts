8db4b05b3ac768a7029e34b746fe0b929fe5a6ef
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2025-02-26 13:49:22 -0600

    Update Angular training app from 18 to 19

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-example';
