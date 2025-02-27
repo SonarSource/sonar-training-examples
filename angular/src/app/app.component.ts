34a21b43137a051e46a511d6c451584168c281af
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2025-02-27 10:15:03 -0600

    Add sample issues for angular/TS project

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -12,10 +12,31 @@ export class AppComponent {
   title = 'angular-example';
 
   doStuff() {
+    const numbers = [10, 2, 30, 1, 5];
+    numbers.sort(); // Noncompliant: lexicographic sort
+    console.log(numbers); // Output: [1, 10, 2, 30, 5]
     alert('I did stuff');
+    function concatenate() {
+      let args = Array.prototype.slice.call(arguments);  // Noncompliant
+      return args.join(', ');
+    }
+
+    function doSomething(isTrue) {
+      var args = Array.prototype.slice.call(arguments, 1); // Noncompliant
+      if (!isTrue) {
+        for (var arg of args) {
+        }
+      }
+    }
   }
 
   doOtherStuff() {
     alert('I did other stuff');
+
+    function f() {
+      try {
+      } catch (err) {
+      }
+    }
   }
 }
