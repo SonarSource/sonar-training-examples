0772b375200c5667cc3cc59d83142d8788e13f63
jtingsanchali <70384906+joe-tingsanchali-sonarsource@users.noreply.github.com>
2025-04-14 11:19:50 -0500

    Update from angular 18 to angular 19 and add more example issues (#171)
    * Update Angular training app from 18 to 19
    
    * Add sample issues for angular/TS project

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,20 +3,40 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-example';
 
   doStuff() {
+    const numbers = [10, 2, 30, 1, 5];
+    numbers.sort(); // Noncompliant: lexicographic sort
+    console.log(numbers); // Output: [1, 10, 2, 30, 5]
     alert('I did stuff');
+    function concatenate() {
+      let args = Array.prototype.slice.call(arguments);  // Noncompliant
+      return args.join(', ');
+    }
+
+    function doSomething(isTrue) {
+      var args = Array.prototype.slice.call(arguments, 1); // Noncompliant
+      if (!isTrue) {
+        for (var arg of args) {
+        }
+      }
+    }
   }
 
   doOtherStuff() {
     alert('I did other stuff');
+
+    function f() {
+      try {
+      } catch (err) {
+      }
+    }
   }
 }
